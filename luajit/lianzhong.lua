3654bef3f4b3fd8c1833ff6bea45b8b387b6e616
Locke <Lockeh@qq.com>
2014-03-08 14:28:56 +0800

    init

---
+++ luajit/lianzhong.lua
@@ -0,0 +1,136 @@
+﻿local ffi = require("ffi")
+
+ffi.cdef[[
+int __stdcall MultiByteToWideChar(int cp, int flag, const char* src, int srclen, char* dst, int dstlen);
+int __stdcall WideCharToMultiByte(int cp, int flag, const char* src, int srclen, char* dst, int dstlen, const char* defchar, int* used);
+
+typedef uintptr_t HANDLE;
+typedef uint32_t DWORD;
+
+HANDLE GetStdHandle(DWORD nStdHandle);
+DWORD GetFileType(HANDLE hFile);
+]]
+
+local function MB_WC(src, cp)
+	local srclen = #src
+	local dstlen = srclen * 2
+	local dst = ffi.new("char[?]", dstlen)
+	dstlen = ffi.C.MultiByteToWideChar(cp or 0, 0, src, srclen, dst, dstlen) * 2
+	return ffi.string(dst, dstlen)
+end
+
+local function WC_MB(src, cp)
+	local srclen = math.floor(#src / 2)
+	local dstlen = srclen * 3
+	local dst = ffi.new("char[?]", dstlen)
+	dstlen = ffi.C.WideCharToMultiByte(cp or 0, 0, src, srclen, dst, dstlen, nil, nil)
+	return ffi.string(dst, dstlen)
+end
+
+local function UTF8_GBK(s)
+	return WC_MB(MB_WC(s, 65001), 936)
+end
+
+--检测stdout是否被重定向
+isstdout = ffi.C.GetFileType(ffi.C.GetStdHandle(-11)) == 2
+
+ffi.cdef[[
+typedef char* LPSTR;
+typedef wchar_t* LPTSTR;
+typedef char* BYTE;
+
+//通过验证码本地路径一键获取结果
+LPSTR  RecYZM(LPSTR strYZMPath, LPSTR strVcodeUser, LPSTR strVcodePass);
+
+//报告验证码错误
+void   ReportError(LPSTR strVcodeUser, LPTSTR strDaMaWorker);
+
+//通过传送字节获取验证码结果
+LPSTR  RecByte(BYTE* byte, int len,LPTSTR strVcodeUser, LPTSTR strVcodePass);
+
+//获取联众剩余点数
+LPSTR  GetUserInfo(LPSTR strUser, LPSTR strPass);
+
+//作者分成函数,通过传送字节获取验证码结果
+LPSTR  RecByte_A(BYTE* byte,int  len,LPSTR strVcodeUser, LPSTR strVcodePass,LPSTR strSoftkey);
+
+//作者分成函数
+LPSTR  RecYZM_A(LPSTR strYZMPath, LPSTR strVcodeUser, LPSTR strVcodePass,LPSTR strSoftkey);
+
+//注册联众账号
+int    Reglz(LPSTR strUser, LPTSTR strPass, LPSTR strEmail, LPSTR strQQ, LPSTR strPhone, LPSTR strSoftUser);
+]]
+
+lianzhong = ffi.load('FastVerCode.dll')
+
+ffi_char = ffi.typeof('char*')
+
+key = '06c9167d7dc9e434d89c1201096c8aee'
+if key ~= nil then
+  ffi_key = ffi.cast(ffi_char, key)
+end
+
+local function GetUserInfo()
+  resstr = ffi.string(lianzhong.GetUserInfo(ffi_name, ffi_pass))
+  res = string.match(resstr, ':(%d+)')
+  if res == nil then
+    res = 0
+  end
+  return tonumber(res)
+end
+
+local function ParseCode(path)
+  local ffi_path = ffi.cast(ffi_char, path)
+  local resstr
+  if ffi_key ~= nil then
+    resstr = ffi.string(lianzhong.RecYZM_A(ffi_path, ffi_name, ffi_pass, ffi_key))
+  else
+    resstr = ffi.string(lianzhong.RecYZM(ffi_path, ffi_name, ffi_pass))
+  end
+  local code, person = string.match(resstr, '(.*)|!|(.*)')
+  
+  if code == nil then
+    local res = {rawerror = '"' .. resstr .. '"'}
+    if resstr == 'No Money!' then
+      res.errorinfo = '余额不足'
+    elseif resstr == 'No Reg!' then
+      res.errorinfo = '登录失败'
+    elseif resstr == 'Error:Put Fail!' then
+      res.errorinfo = '上传验证码失败'
+    elseif resstr == 'Error:TimeOut!' then
+      res.errorinfo = '识别超时'
+    elseif resstr == 'Error:empty picture!' then
+      res.errorinfo = '上传无效验证码'
+    end
+    return res
+  elseif code == 'Error:TimeOut!' then
+    return {errorinfo = '识别超时', rawerror = 'Error:TimeOut!', raw=resstr, person=person}
+  end
+  return {code=code, person=person}
+end
+
+if #arg == 0 then
+  print('Show left money: username password')
+  print('Parse image code: username password imagepath')
+elseif #arg == 2 then
+  username = arg[1]
+  password = arg[2]
+  ffi_name = ffi.cast(ffi_char, username)
+  ffi_pass = ffi.cast(ffi_char, password)
+  print('moneyleft', '=', GetUserInfo())
+elseif #arg == 3 then
+  username = arg[1]
+  password = arg[2]
+  ffi_name = ffi.cast(ffi_char, username)
+  ffi_pass = ffi.cast(ffi_char, password)
+  
+  local res = ParseCode(arg[3])
+  res.moneyleft = GetUserInfo()
+
+  if isstdout and res.errorinfo then
+    res.errorinfo = UTF8_GBK(res.errorinfo)
+  end
+  for key, value in pairs(res) do
+    print(key, '=', value)
+  end
+end
---
+++ python3/LianzhongCAPTCHA.py
@@ -0,0 +1,161 @@
+﻿
+# -*- coding: utf-8 -*-
+
+import sys
+import re
+import ctypes
+
+c_char_p = ctypes.c_char_p
+c_void_p = ctypes.c_void_p
+c_int = ctypes.c_int
+
+if sys.maxsize > 2**32:
+    lzDll = ctypes.cdll.LoadLibrary('FastVerCode64.dll')
+else:
+    lzDll = ctypes.cdll.LoadLibrary('FastVerCode.dll')
+    
+lzGetUserInfo = lzDll.GetUserInfo_A
+lzGetUserInfo.argtypes = [c_char_p, c_char_p, c_char_p]
+lzGetUserInfo.restype = c_char_p
+
+lzReportError = lzDll.ReportError
+lzReportError.argtypes = [c_char_p, c_char_p]
+
+lzRecYZM = lzDll.RecYZM_A_2
+lzRecYZM.argtypes = [c_char_p, c_char_p, c_char_p, c_int, c_int, c_int, c_char_p]
+lzRecYZM.restype = c_char_p
+
+lzRecByte = lzDll.RecByte_A_2
+lzRecByte.argtypes = [c_char_p, c_int, c_char_p, c_char_p, c_int, c_int, c_int, c_char_p]
+lzRecByte.restype = c_char_p
+
+class lzLoginException(Exception):
+    pass
+
+class lzNoMoneyException(Exception):
+    pass
+
+class lzWrongImageException(Exception):
+    pass
+
+class lzUploadException(Exception):
+    pass
+    
+class lzTimeoutException(Exception):
+    pass
+
+class lzClientWrongResult(Exception):
+    pass
+
+class lzClientUnknownResultException(Exception):
+    def __init__(self, res):
+        self.res = res
+    def __str__(self):
+        return 'Unknown: ' + res
+    def __repr__(self):
+        return 'Unknown: ' + res
+
+def GetMoney(name, password, softwareKey = ''):
+    c_name = c_char_p(name.encode('gbk'))
+    c_pass = c_char_p(password.encode('gbk'))
+    c_key = c_char_p(softwareKey.encode('gbk'))
+    res = lzGetUserInfo(c_name, c_pass, c_key).decode('gbk')
+    m = re.match(r'亲爱的联众用户，您当前剩余点数:(\d+)', res)
+    if m: return int(m.group(1))
+    if res == '亲爱的联众用户，您的密码错误!':
+        raise lzLoginException()
+    return lzClientUnknownResultException(res)
+
+def ReportError(name, worker):
+    c_name = c_char_p(name.encode('gbk'))
+    c_worker = c_char_p(worker.encode('gbk'))
+    lzReportError(c_name, c_worker)
+
+def _clientParseResponse(res):
+    m = re.match('(.+?)\|!\|(.+)', res)
+    if m:
+        code = m.group(1)
+        worker = m.group(2)
+        if code == 'Error:TimeOut!':
+            raise lzTimeoutException()
+        return (code, worker)
+    if res == 'No Money!':
+        raise lzNoMoneyException()
+    if res == 'No Reg!':
+        raise lzLoginException()
+    if res == 'Error:Put Fail!':
+        raise lzUploadException()
+    if res == 'Error:TimeOut!':
+        raise lzTimeoutException()
+    if res == 'Error:empty picture!':
+        raise lzWrongImageException()
+    return lzClientUnknownResultException(res)
+    
+
+def ParseImageFile(name, password, filepath, softwareKey = '', minLen=0, maxLen=0, type=0):
+    c_name = c_char_p(name.encode('gbk'))
+    c_pass = c_char_p(password.encode('gbk'))
+    c_key = c_char_p(softwareKey.encode('gbk'))
+    c_path = c_char_p(filepath.encode('gbk'))
+    res = lzRecYZM(c_path, c_name, c_pass, type, minLen, maxLen, c_key).decode('gbk')
+    return _clientParseResponse(res)
+    
+def ParseImageBytes(name, password, bytes, softwareKey = '', minLen=0, maxLen=0, type=0):
+    c_name = c_char_p(name.encode('gbk'))
+    c_pass = c_char_p(password.encode('gbk'))
+    c_key = c_char_p(softwareKey.encode('gbk'))
+    c_bytes = c_char_p(bytes)
+    c_len = c_int(len(bytes))
+    res = lzRecByte(c_bytes, c_len, c_name, c_pass, type, minLen, maxLen, c_key).decode('gbk')
+    return _clientParseResponse(res)
+
+class LzCodeResult:
+    def __init__(self, code, name, worker, failCount):
+        self.code = code
+        self.name = name
+        self.worker = worker
+        self.failCount = failCount
+        self.success = code != None
+    
+    def reportWrong(self):
+        if self.success:
+            ReportError(self.name, self.worker)
+    
+    def __str__(self):
+        return self.code
+
+class LzClient:
+    def __init__(self, username, password, sortwareKey = ''):
+        self.name = username
+        self.password = password
+        self.key = sortwareKey
+    
+    def getMoney(self):
+        return GetMoney(self.name, self.password, self.key)
+    
+    def parseImage(self, imagePathOrBytes, retry=0, minLen=0, maxLen=0, codelen=0, type=0):
+        if codelen != 0 and minLen == 0 and maxLen == 0:
+            minLen = codelen
+            maxLen = codelen
+        
+        failCount = 0
+        while True:
+            try:
+                if isinstance(imagePathOrBytes, str):
+                    res = ParseImageFile(self.name, self.password, imagePathOrBytes, self.key, minLen=minLen, maxLen=maxLen, type=type)
+                elif isinstance(imagePathOrBytes, bytes):
+                    res = ParseImageBytes(self.name, self.password, imagePathOrBytes, self.key, minLen=minLen, maxLen=maxLen, type=type)
+                else:
+                    raise lzWrongImageException()
+                code = res[0]
+                worker = res[1]
+                if (minLen !=0 and len(code) < minLen) or (maxLen!=0 and len(code) > maxLen):
+                    ReportError(self.name, worker)
+                    raise lzClientWrongResult()
+                return LzCodeResult(code, self.name, worker, failCount)
+            except (lzUploadException, lzTimeoutException, lzClientWrongResult):
+                retry -= 1
+                failCount += 1
+                if retry < 0:
+                    return LzCodeResult(None, self.name, None, failCount)
+
